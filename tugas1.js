4ab6a12b2cb4179e29511660937639fc67c8ea40
yohanes_prima <jooprima@gmail.com>
2019-07-14 22:23:05 +0700

    Tugas 1 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var namaDepan = "Yohanes";
+let namaBelakang = "Prima";
+const umur = 28;
+
+console.log(namaDepan + " " + namaBelakang + " " + umur);
+// console.log(namaBelakang);
+// console.log(umur);
